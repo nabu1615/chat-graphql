9393fe21140cbcba2d6ba4f03ea6bc2521beec0f
nabu1615 <javsdev@gmail.com>
2023-10-14 11:14:49 -0500

    Basic chat structure files

---
+++ client/src/lib/graphql/hooks.js
@@ -0,0 +1,29 @@
+import { useMutation, useQuery } from '@apollo/client';
+import { addMessageMutation, messagesQuery } from './queries';
+
+export function useAddMessage() {
+  const [mutate] = useMutation(addMessageMutation);
+
+  const addMessage = async (text) => {
+    const { data: { message } } = await mutate({
+      variables: { text },
+      update: (cache, { data: { message } }) => {
+        cache.updateQuery({ query: messagesQuery }, ({ messages }) => {
+          return {
+            messages: [...messages, message],
+          }
+        })
+      }
+    });
+    return message;
+  };
+
+  return { addMessage };
+}
+
+export function useMessages() {
+  const { data } = useQuery(messagesQuery);
+  return {
+    messages: data?.messages ?? [],
+  };
+}
